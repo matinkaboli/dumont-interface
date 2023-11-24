85a58af9256e9447bda85920b8a78e772eaf2610
mersedee <mersede.rahbar@gmail.com>
2023-11-24 16:33:48 +0330

    feat: build button component

---
+++ src/components/core/Button/index.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '.';
+
+const meta = {
+  title: 'core/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    size: 'lg',
+    radius: "lg",
+    label: 'button'
+  },
+};
---
+++ src/components/core/Button/index.tsx
@@ -0,0 +1,46 @@
+import { type ButtonHTMLAttributes, type FC } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const buttonVariants = cva(
+  '',
+  {
+    variants: {
+      variant: {
+        primary: '',
+        secondary: '',
+        warning: '',
+        neutral: '',
+        link: '',
+      },
+      size: {
+        lg: '',
+        md: '',
+        sm: '',
+      },
+      radius: {
+        lg: '',
+        md: '',
+        sm: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'lg',
+      radius: 'lg',
+    },
+  },
+);
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  label: string;
+}
+
+const Button: FC<ButtonProps> = ({ className, variant, size, label, ...props }) => (
+  <button className={buttonVariants({ variant, size, className })} {...props}>
+    {label}
+  </button>
+);
+
+export default Button;
