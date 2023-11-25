8a8e6a8d4758ed5321efd3f593fe6298f8d335a6
mersedee <mersede.rahbar@gmail.com>
2023-11-25 11:05:52 +0330

    chore: Config tailwind theme

--- src/components/core/Button/index.tsx
+++ src/components/core/Button/index.tsx
@@ -6,7 +6,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        primary: 'bg-blue-400 text-white',
+        primary: 'bg-neutrals-50 text-black',
         secondary: '',
         warning: '',
         neutral: '',
