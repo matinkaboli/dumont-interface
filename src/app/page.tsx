fe39cc1c4554a343b7b1cbe7ba9b736688f93323
mersedee <mersede.rahbar@gmail.com>
2023-12-18 15:03:00 +0330

    style(layout): Implement gradient border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-
 export default function Home() {
-  return (
-    <div className="text-white">
-      Some homePage content
-    </div>
-  );
+  return <div className="text-white">Some homePage content</div>;
 }
--- src/components/Layout/Header/ConnectWallet/GiftButton.tsx
+++ src/components/Layout/Header/ConnectWallet/GiftButton.tsx
@@ -2,12 +2,14 @@ import { Icon } from '@/components';
 
 const GiftButton = () => {
   return (
-    <button type="button" className="bg-primary-800 border border-primary-400 rounded-lg px-2 h-10">
-      <span className="relative">
-        <Icon name="gift-fill" />
-        <span className="h-2 w-2 rounded-full absolute right-0 bottom-0 bg-primary-200"></span>
-      </span>
-    </button>
+    <div className="border-primary-gradiant rounded-lg">
+      <button type="button" className="bg-primary-800 rounded-lg px-2 h-10">
+        <span className="relative">
+          <Icon name="gift-fill" />
+          <span className="h-2 w-2 rounded-full absolute right-0 bottom-0 bg-primary-200"></span>
+        </span>
+      </button>
+    </div>
   );
 };
 
