7044f62ea96c3f92e63d34254a703261f507fb23
mersedee <mersede.rahbar@gmail.com>
2023-12-23 10:52:33 +0330

    fix: Enforce Optimism network

--- src/providers/ConnectKit.tsx
+++ src/providers/ConnectKit.tsx
@@ -3,12 +3,16 @@
 import { type PropsWithChildren } from 'react';
 import { createConfig, WagmiConfig } from 'wagmi';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+import { optimism } from 'wagmi/chains';
+
+const chains = [optimism];
 
 const config = createConfig(
   getDefaultConfig({
     alchemyId: process.env.ALCHEMY_ID,
     walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID!,
     appName: 'Dumont',
+    chains,
   }),
 );
 
