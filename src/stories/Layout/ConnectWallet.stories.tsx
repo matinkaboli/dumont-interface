520106c903eebee338fd3357baf2cdc2e20d80d0
mersedee <mersede.rahbar@gmail.com>
2023-12-25 20:11:49 +0330

    docs: Add global decorators for providers

--- src/stories/Layout/ConnectWallet.stories.tsx
+++ src/stories/Layout/ConnectWallet.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import ConnectWallet from '@/components/Layout/Header/ConnectWallet';
-import ConnectKit from '@/providers/ConnectKit';
 
 const meta = {
   title: 'Layout/ConnectWallet',
@@ -11,13 +10,6 @@ const meta = {
     inspectComponents: [ConnectWallet],
   },
   tags: ['autodocs'],
-  decorators: [
-    (Story) => (
-      <ConnectKit>
-        <Story />
-      </ConnectKit>
-    ),
-  ],
 } satisfies Meta<typeof ConnectWallet>;
 
 export default meta;
--- src/stories/Layout/Round.stories.tsx
+++ src/stories/Layout/Round.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Round from '@/components/Layout/Footer/Round';
-import ConnectKit from '@/providers/ConnectKit';
 
 const meta = {
   title: 'Layout/Round',
@@ -11,13 +10,6 @@ const meta = {
     inspectComponents: [Round],
   },
   tags: ['autodocs'],
-  decorators: [
-    (Story) => (
-      <ConnectKit>
-        <Story />
-      </ConnectKit>
-    ),
-  ],
 } satisfies Meta<typeof Round>;
 
 export default meta;
