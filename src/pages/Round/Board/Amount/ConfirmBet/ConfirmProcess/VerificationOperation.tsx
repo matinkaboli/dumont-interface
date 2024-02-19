128049af1a40428251cc763e873bac4bedf4c516
mersedee <mersede.rahbar@gmail.com>
2024-02-19 09:34:56 +0330

    fix: Implement verification operation message

--- src/pages/Round/Board/Amount/ConfirmBet/ConfirmProcess/VerificationOperation.tsx
+++ src/pages/Round/Board/Amount/ConfirmBet/ConfirmProcess/VerificationOperation.tsx
@@ -1,9 +1,9 @@
 const VerificationOperation = () => {
   return (
-    <div className="px-8">
+    <div className="text-center px-8">
       <div className="w-full h-[136px] rounded-xl bg-white" />
       <h4 className="text-md text-white font-medium mt-6">Verifying the operator&apos;s proof</h4>
-      <p className="text-sm text-neutral-300 mt-2"></p>
+      <p className="text-sm text-neutral-300 mt-2">This may take a few seconds</p>
     </div>
   );
 };
