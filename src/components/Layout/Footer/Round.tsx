f6e4fdc02ad067dca59fd66d0b10e7b8b77d3b08
mersedee <mersede.rahbar@gmail.com>
2024-02-29 15:22:44 +0330

    refactor: Update footer when the round is not created

--- src/components/Layout/Footer/Round.tsx
+++ src/components/Layout/Footer/Round.tsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components';
 
 interface Props {
   roundTime?: string;
@@ -8,22 +7,13 @@ interface Props {
 const Round = ({ roundTime }: Props) => {
   const iconStyles = roundTime ? 'bg-primary-200' : 'bg-neutral-500';
   const textStyles = roundTime ? 'text-white' : 'text-neutral-200';
-  const labelText = roundTime ? roundTime : 'There is no round';
+  const labelText = roundTime ? roundTime : 'No rounds created';
 
   return (
-    <TooltipProvider delayDuration={100}>
-      <Tooltip>
-        <TooltipTrigger>
-          <div className="flex-center-v gap-1">
-            <div className={clsx('w-1.5 h-1.5 rounded-full', iconStyles)} />
-            <div className={clsx('text-sm font-medium', textStyles)}>{labelText}</div>
-          </div>
-        </TooltipTrigger>
-        <TooltipContent className="w-48 !text-xs">
-          Each round has an expiration time. After that it becomes inactive.
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div className="flex-center-v gap-1">
+      <div className={clsx('w-1.5 h-1.5 rounded-full', iconStyles)} />
+      <div className={clsx('text-sm font-medium', textStyles)}>{labelText}</div>
+    </div>
   );
 };
 
