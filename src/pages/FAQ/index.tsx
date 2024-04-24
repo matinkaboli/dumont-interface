017c4d531d1070deb0e6252de61f5e3d4a0c5ce2
mersedee <mersede.rahbar@gmail.com>
2024-04-24 10:09:15 +0330

    feat: Implement faq page components

--- src/pages/FAQ/index.tsx
+++ src/pages/FAQ/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Accordion, Container, Layout, Typography } from '@/components';
+
+import CTASection from '@/pages/_components/CTASection';
 
 const FAQ = () => {
   return (
-    <div>
-      FAQ page
-    </div>
+    <Layout>
+      <Container className="pt-28">
+        <Typography tag="h1" variant="title1" className="text-center">
+          Frequently asked questions
+        </Typography>
+        <Typography tag="p" variant="body1" className="text-center mt-4">
+          Have any question? We’re here to help
+        </Typography>
+
+        <Accordion />
+
+        <CTASection />
+      </Container>
+    </Layout>
   );
 };
 
