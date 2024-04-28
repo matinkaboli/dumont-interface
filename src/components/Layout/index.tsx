559b7b2396534d822fdfccea6fca116e34f83c53
mersedee <mersede.rahbar@gmail.com>
2024-04-28 10:20:00 +0330

    feat: Implement smooth scroll

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,21 +3,24 @@ import clsx from 'clsx';
 
 import Header from './Header';
 import Footer from './Footer';
+import SmoothScroll from './SmoothScroll';
 
 const bgGradiantClassName = 'absolute left-0 right-0 bg-gradiant-primary-top blur-[40px] opacity-70 w-screen h-[243px]';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="relative">
-      <div className={clsx('top-0', bgGradiantClassName)} />
-      <div className={clsx('bottom-0', bgGradiantClassName)} />
+    <SmoothScroll>
+      <div className='relative'>
+        <div className={clsx('top-0', bgGradiantClassName)} />
+        <div className={clsx('bottom-0', bgGradiantClassName)} />
 
-      <div className='relative h-full pt-8'>
-        <Header />
-        {children}
-        <Footer />
+        <div className='relative h-full pt-8'>
+          <Header />
+          {children}
+          <Footer />
+        </div>
       </div>
-    </div>
+    </SmoothScroll>
   );
 };
 
