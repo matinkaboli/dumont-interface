1fcc2985d54d5a983adfdd746cb7eefefcce19cc
mersedee <mersede.rahbar@gmail.com>
2024-05-09 17:17:12 +0330

    feat: Fetch game data on round page

--- src/redux/features/gameSlice.ts
+++ src/redux/features/gameSlice.ts
@@ -1,45 +1,76 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
 import makeApiUrl from '@/helpers/makeApiUrl';
 
 interface State {
   loading: boolean;
   error: string | null;
+  isSaved: boolean;
   data: any;
 }
 
-const initialState: State = {
-  loading: false,
-  error: null,
-  data: null,
-};
+export const postGame = createAsyncThunk(
+  'api/postGame',
+  async (requestData: any, { rejectWithValue }) => {
+    try {
+      const url = makeApiUrl('games');
+      const response = await axios.post(url, requestData);
+      return response.data.result;
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      return rejectWithValue(axiosError.message);
+    }
+  },
+);
 
-export const postGame = createAsyncThunk('api/postData', async (requestData: any, {rejectWithValue}) => {
+export const getGame = createAsyncThunk('api/getGame', async (id: string, { rejectWithValue }) => {
   try {
-    const url = makeApiUrl('games');
-    const response = await axios.post(url, requestData);
-    return response.data;
+    const url = makeApiUrl(`games/${id}`);
+    const response = await axios.get(url);
+    return response.data.result;
   } catch (error) {
-    // @ts-ignore
-    return rejectWithValue(error.message);
+    const axiosError = error as AxiosError;
+    return rejectWithValue(axiosError.message);
   }
 });
 
 const gameSlice = createSlice({
   name: 'game',
-  initialState,
+  initialState: {
+    loading: false,
+    error: null,
+    isSaved: false,
+    data: null,
+  } as State,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(postGame.pending, (state) => {
         state.loading = true;
+        state.isSaved = false;
         state.error = null;
       })
       .addCase(postGame.fulfilled, (state, action) => {
         state.loading = false;
+        state.isSaved = true;
         state.data = action.payload;
       })
       .addCase(postGame.rejected, (state, action) => {
+        state.loading = false;
+        state.isSaved = false;
+        state.error = action.payload as string;
+      });
+    builder
+      .addCase(getGame.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getGame.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(getGame.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload as string;
       });
--- src/views/Round/index.tsx
+++ src/views/Round/index.tsx
@@ -1,22 +1,30 @@
 'use client';
 
-import { redirect } from 'next/navigation';
+import { redirect, useParams } from 'next/navigation';
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
 
 import { useTypedSelector } from '@/hooks/useTypedSelector';
 import CardDeck from '@/views/_components/CardDeck';
 import Board from '@/views/_components/Board';
 import { Toast, ToastContent } from '@/components';
+import { AppDispatch } from '@/redux/store';
+import { getGame } from '@/redux/features/gameSlice';
 
 import ActivityTab from './ActivityTab';
 import ProgressbarTimer from './ProgressbarTimer';
 
 const CreateRound = () => {
+  const params = useParams();
+  const dispatch = useDispatch<AppDispatch>();
   const { isConfirmed } = useTypedSelector((state) => state.createRound);
   const { isConnected } = useTypedSelector((state) => state.account.profile);
 
   useEffect(() => {
+    const id = params.id as string;
+    dispatch(getGame(id));
+
     toast(
       <ToastContent
         variant="neutral"
@@ -37,7 +45,9 @@ const CreateRound = () => {
 
   return (
     <>
-      <div className="px-1.5"><ProgressbarTimer /></div>
+      <div className="px-1.5">
+        <ProgressbarTimer />
+      </div>
 
       <div className="flex flex-col gap-4">
         <CardDeck />
