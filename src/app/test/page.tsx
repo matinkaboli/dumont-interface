d3d378bd4a33002595f68aa72eef0e9409cf51b6
mersedee <mersede.rahbar@gmail.com>
2024-06-25 13:36:23 +0330

    feat: Build Confetti component

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Confetti from '@/components/Confetti';
+
+const Page = () => {
+  return (
+    <div>
+      <Confetti run={true} />
+    </div>
+  );
+};
+
+export default Page;
