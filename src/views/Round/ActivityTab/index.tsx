36fc5f0e4f8e8cb4445d0697861cbee3ef11d95b
mersedee <mersede.rahbar@gmail.com>
2024-06-27 12:43:35 +0330

    feat: Update activity tab id

--- src/views/Round/ActivityTab/index.tsx
+++ src/views/Round/ActivityTab/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components';
 
 import Activities from './Activities';
--- src/views/_components/Board/KeyBoard/Keys/RevealKey/index.tsx
+++ src/views/_components/Board/KeyBoard/Keys/RevealKey/index.tsx
@@ -81,7 +81,9 @@ const RevealKey = ({ className }: { className?: string }) => {
       })
       .catch(() => {
         onError();
-      });
+      }).finally(() => {
+        console.log('finished');
+    });
   }
 
   function onError() {
@@ -109,7 +111,7 @@ const RevealKey = ({ className }: { className?: string }) => {
       className="flex flex-col gap-0.5 disabled:bg-neutral-800 disabled:border-neutral-750 [&>div]:disabled:text-neutral-500"
       borderClassName={clsx('col-span-2', className)}
       onClick={onReveal}
-      disabled={!game?.id || game.cards[activeCardIndex]?.isLeaked || leakedCount === 0}
+      disabled={!game?.id || game.cards[activeCardIndex]?.isLeaked}
     >
       <div className="text-md text-white font-bold">Reveal {`->`}</div>
       <div className="text-neutral-500 text-sm">{leakedCount} / 3 remaining</div>
