6664496e23c976dd0589fc61a25878090b8aa177
mersedee <mersede.rahbar@gmail.com>
2024-07-10 10:45:38 +0330

    style: Adjust reward button height

--- src/components/Layout/Header/ConnectWallet/RewardButton.tsx
+++ src/components/Layout/Header/ConnectWallet/RewardButton.tsx
@@ -1,13 +1,13 @@
 import { Icon } from '@/components';
 
-const GiftButton = () => {
+const RewardButton = () => {
   return (
     <div className="border-primary-gradiant rounded-lg">
-      <button type="button" className="bg-primary-800 rounded-lg px-2 h-10">
+      <button type="button" className="flex-center-v bg-primary-800 rounded-lg px-2 h-10">
         <Icon name="gift-rainbow" />
       </button>
     </div>
   );
 };
 
-export default GiftButton;
+export default RewardButton;
