eb3eb6ee799fac48d35513cb59cb968b7e8e94e9
mersedee <mersede.rahbar@gmail.com>
2024-07-18 12:24:53 +0330

    feat: Redirect player if they have active game

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import axios from 'axios';
-import { useParams, usePathname } from 'next/navigation';
 
+import { getPlayerGames, redirectPlayer } from '@/redux/features/accountSlice';
+import { AppDispatch } from '@/redux/store';
+import timeLeftInSeconds from '@/helpers/timeLeftInSeconds';
+import makeApiUrl from '@/helpers/makeApiUrl';
 import { useTypedSelector } from '@/hooks/useTypedSelector';
+
 import Board from '@/views/_components/Board';
 import CardDeck from '@/views/_components/CardDeck';
-import makeApiUrl from '@/helpers/makeApiUrl';
 
 import CreateRound from './Create';
 
@@ -23,9 +28,16 @@ const fetchReferrerAddress = async (id: string) => {
 };
 
 const Home = () => {
-  const { isConnected, isConnecting } = useTypedSelector((state) => state.account.profile);
   const params = useParams();
   const pathname = usePathname();
+  const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+  const {
+    profile: { isConnected, isConnecting, address },
+    isRedirected,
+    loading,
+  } = useTypedSelector((state) => state.account);
+  const [localLoading, setLocalLoading] = useState<boolean>(true);
 
   const hasReferralId = useMemo(() => params?.id && pathname.includes('/i/'), [params, pathname]);
 
@@ -35,7 +47,31 @@ const Home = () => {
     }
   }, [hasReferralId]);
 
-  if (isConnecting) return <div className="text-center text-white mt-16">Loading...</div>;
+  useEffect(() => {
+    if (address) {
+      dispatch(getPlayerGames(address))
+        .unwrap()
+        .then((result) => {
+          const timeLeft = +result[0].duration - timeLeftInSeconds(result[0].createdAt);
+          if (timeLeft > 0 && !isRedirected) {
+            dispatch(redirectPlayer(true));
+            router.push(`/${result[0].id}`);
+          }
+        })
+        .finally(() => {
+          setLocalLoading(false);
+        });
+    }
+  }, [address]);
+
+  if (isConnecting || loading || localLoading)
+    return <div className="text-center text-white mt-16">Loading...</div>;
+
+  // if (!isRedirected && redirectId ) {
+  //   // dispatch(redirectPlayer(true));
+  //
+  //   redirect(`/${redirectId}`);
+  // }
 
   return (
     <div className="flex flex-col gap-4">
