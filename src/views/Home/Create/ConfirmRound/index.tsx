c058c36ffe1afa22b831efdc588b179dbcfe8637
mersedee <mersede.rahbar@gmail.com>
2024-07-21 14:52:03 +0330

    fix: Include referral address in game creation request

--- src/views/Home/Create/ConfirmRound/index.tsx
+++ src/views/Home/Create/ConfirmRound/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
 import { useContractWrite, useWaitForTransaction } from 'wagmi';
 import BN from 'bignumber.js';
+import axios from 'axios';
 
 import { Button } from '@/components';
 import { closeDialog, openDialog, updateDialogContent } from '@/redux/features/dialogSlice';
@@ -13,6 +14,7 @@ import { useApproval } from '@/hooks/useApproval';
 import extractGameId from '@/helpers/extractGameId';
 import GAME_FACTORY_ABI from '@/abis/GAME_FACTORY_ABI.json';
 import formatUnits from '@/helpers/formatUnits';
+import makeApiUrl from '@/helpers/makeApiUrl';
 
 import AnimatedDialogContent from '@/views/_components/AnimatedDialogContent';
 import LoadingContent from '@/views/_components/Dialog/LoadingContent';
@@ -26,10 +28,13 @@ const approveValue = '1';
 
 const ConfirmRound = () => {
   const router = useRouter();
+  const params = useParams();
+  const pathname = usePathname();
   const dispatch = useDispatch<AppDispatch>();
   const { details } = useTypedSelector((state) => state.config);
   const [redirectId, setRedirectId] = useState(0);
   const [activeIndex, setActiveIndex] = useState(0); // The active index corresponds to the index in the long loading array.
+  const [referralAddress, setReferralAddress] = useState();
   const { allowanceData, sendApprove, isApproveLoading } = useApproval(
     details?.gameFactory,
     onApproveSuccess,
@@ -44,7 +49,7 @@ const ConfirmRound = () => {
     address: details?.gameFactory,
     abi: GAME_FACTORY_ABI,
     functionName: 'createGame',
-    args: ['0x0000000000000000000000000000000000000000'],
+    args: [referralAddress ?? '0x0000000000000000000000000000000000000000'],
     onError: () => onError('Creating was unsuccessful', onCreateGame),
     onSuccess: () => setActiveIndex(1),
   });
@@ -56,6 +61,22 @@ const ConfirmRound = () => {
     onSettled: onCreateGameSettled,
     onError: () => onError('Creating was unsuccessful', onCreateGame),
   });
+
+  useEffect(() => {
+    const hasReferralId = params?.id && pathname.includes('/i/');
+    const getReferral = async () => {
+      if (hasReferralId) {
+        try {
+          const response = await axios.get(makeApiUrl(`referrals/${params.id}`));
+          setReferralAddress(response?.data?.result?.address);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+    };
+
+    getReferral();
+  }, [params.id, pathname]);
 
   useEffect(() => {
     if (isApproveLoading || isCreateGameLoading) {
@@ -151,7 +172,10 @@ const ConfirmRound = () => {
     onError('Approve was unsuccessful', () => sendApprove(approveValue));
   }
 
-  const onCreateGame = () => writeCreateGame?.();
+  const onCreateGame = () => {
+    console.log(referralAddress)
+    writeCreateGame?.();
+  };
 
   const onCreateRound = () => {
     const isApproved = new BN(allowanceData as string).isGreaterThanOrEqualTo(
