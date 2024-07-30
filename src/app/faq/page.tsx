af1b510d887c6f4fca00b52bf0ea0d39b189c2b7
mersedee <mersede.rahbar@gmail.com>
2024-07-30 10:48:39 +0330

    feat: Update FAQ page title

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
+
 import FAQ from '@/views/FAQ';
+
+export const metadata: Metadata = {
+  title: 'FAQ | Dumont',
+};
 
 export default function FaqPage() {
   return <FAQ />;
