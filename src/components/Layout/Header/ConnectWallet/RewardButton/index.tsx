c05bc13ffdfefdc660e6350e0f112bf579d731cb
mersedee <mersede.rahbar@gmail.com>
2024-08-05 12:48:56 +0330

    fix: Fix reward value display

--- src/components/Layout/Header/ConnectWallet/RewardButton/index.tsx
+++ src/components/Layout/Header/ConnectWallet/RewardButton/index.tsx
@@ -95,13 +95,6 @@ const RewardButton = () => {
     );
   }
 
-  function updateClaimValue(value: BigNumber) {
-    const claimValue = parseUnits(value, 18);
-    dispatch(
-      updateDialogContent(<ClaimReward claimValue={claimValue.toString()} onClaim={onClaim} />),
-    );
-  }
-
   function onOpenDialog() {
     const initialValue = parseUnits(balancesData as BigNumber, 18);
 
@@ -114,7 +107,9 @@ const RewardButton = () => {
     refetetchBalances().then((newBalance) => {
       if (balancesData !== newBalance.data) {
         const newValue = parseUnits(newBalance.data as BigNumber, 18);
-        updateClaimValue(newValue);
+        dispatch(
+          updateDialogContent(<ClaimReward claimValue={newValue.toString()} onClaim={onClaim} />),
+        );
       }
     });
   }
