0470323c1587d93b2ee01993261fa42267dfc063
mersedee <mersede.rahbar@gmail.com>
2024-08-25 08:54:39 +0330

    feat: Add start page

---
+++ src/views/Start/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Loading } from '@/components';
+import { useTypedSelector } from '@/hooks/useTypedSelector';
+
+import Board from '@/views/_components/Board';
+import CreateRound from '@/views/_components/CreateRound';
+
+const Start = () => {
+  const {
+    loading,
+    profile: { isConnecting },
+  } = useTypedSelector((state) => state.account);
+
+  if (isConnecting || loading)
+    return (
+      <div className="min-h-[50vh] flex-center">
+        <Loading />
+      </div>
+    );
+
+  return (
+    <div className="flex flex-col gap-4">
+      <CreateRound />
+
+      <Board />
+    </div>
+  );
+};
+
+export default Start;
