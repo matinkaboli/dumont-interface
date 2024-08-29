fc2a5e86c2c3885df4f23878fd0c5ca88d474df4
mersedee <mersede.rahbar@gmail.com>
2024-08-29 09:43:32 +0330

    fix: Fix tooltip auto focus

--- src/views/_components/InfoTooltip/index.tsx
+++ src/views/_components/InfoTooltip/index.tsx
@@ -6,18 +6,19 @@ interface Props {
   label?: string | ReactNode;
   tooltipText?: string;
   className?: string;
+  bgColor?: string;
 }
 
-const InfoTooltip = ({ label, tooltipText, className }: Props) => {
+const InfoTooltip = ({ label, tooltipText, className, bgColor = '#111117' }: Props) => {
   return (
     <div className="flex items-center gap-0.5">
       <div className={className}>{label}</div>
       <TooltipProvider delayDuration={100}>
         <Tooltip>
-          <TooltipTrigger>
+          <TooltipTrigger onFocus={(event) => event.preventDefault()}>
             <Icon name="circle-exclamation-fill" />
           </TooltipTrigger>
-          <TooltipContent>{tooltipText}</TooltipContent>
+          <TooltipContent bgColor={bgColor}>{tooltipText}</TooltipContent>
         </Tooltip>
       </TooltipProvider>
     </div>
