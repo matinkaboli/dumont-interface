f52b18dd430ab933ee0e8381e531a92dc00da49d
Matin Kaboli <matinkaboli@aol.com>
2024-09-10 17:21:36 +0330

    fix: isClaimable function for requestedAt

--- src/views/Round/ActivityTab/helpers/isClaimable.ts
+++ src/views/Round/ActivityTab/helpers/isClaimable.ts
@@ -1,9 +1,8 @@
 import dayjs from 'dayjs';
 
-const isClaimable = (requestedAt: string, claimableAfter?: string) => {
-  const requested = parseInt(requestedAt, 10); // Convert to number
+const isClaimable = (requestedAt: number, claimableAfter?: string) => {
   const claimable = parseInt(claimableAfter ?? '0', 10);
-  const claimableTime = dayjs.unix(requested + claimable);
+  const claimableTime = dayjs.unix(requestedAt + claimable);
   const currentTime = dayjs();
 
   return currentTime.isAfter(claimableTime);
