b90dced0e715d268c43749ab5994a9c3fd5a7b81
mersedee <mersede.rahbar@gmail.com>
2024-09-10 11:46:48 +0330

    feat: Increase confetti animation speed

--- src/components/Layout/Header/ConnectWallet/RewardButton/index.tsx
+++ src/components/Layout/Header/ConnectWallet/RewardButton/index.tsx
@@ -130,7 +130,13 @@ const RewardButton = () => {
   return (
     <>
       {claimed ? (
-        <Confetti run onConfettiComplete={onConfettiComplete} className="!z-[45]" />
+        <Confetti
+          run
+          gravity={0.2}
+          tweenDuration={2000}
+          onConfettiComplete={onConfettiComplete}
+          className="!z-[45]"
+        />
       ) : null}
 
       <div className="border-primary-gradiant rounded-lg">
