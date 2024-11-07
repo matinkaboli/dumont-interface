73d9b77a336e436e0063bf3e037978ba19f97d39
mersedee <mersede.rahbar@gmail.com>
2024-11-07 10:08:48 +0330

    feat: Add network to privy setting

--- src/providers/PrivyAuth.tsx
+++ src/providers/PrivyAuth.tsx
@@ -2,6 +2,10 @@
 
 import { ReactNode } from 'react';
 import { PrivyProvider } from '@privy-io/react-auth';
+import { base, baseSepolia } from 'wagmi/chains';
+import { Networks } from '@/types';
+
+const network = process.env.NEXT_PUBLIC_NETWORK as Networks;
 
 export default function Privy({ children }: { children: ReactNode }) {
   return (
@@ -16,6 +20,7 @@ export default function Privy({ children }: { children: ReactNode }) {
         embeddedWallets: {
           createOnLogin: 'users-without-wallets',
         },
+        supportedChains: [network === 'baseSepolia' ? baseSepolia : base],
       }}
     >
       {children}
