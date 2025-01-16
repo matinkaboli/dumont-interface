6027aaf4afc9820d6a5251955ae6912f939115f6
mersedee <mersede.rahbar@gmail.com>
2025-01-16 09:22:46 +0330

    fix: Fix OG image url

--- src/app/rounds/[id]/page.tsx
+++ src/app/rounds/[id]/page.tsx
@@ -1,31 +1,41 @@
 import Round from '@/views/Round';
 
-const website = 'https://app.dumont.gg';
-
-export const metadata = {
-  title: 'Ultimate Card Game Strategy Guide',
-  description:
-    'Master the art of card games with our expert guide. Learn strategies, rules, and tips to dominate any deck.',
-  openGraph: {
-    title: 'The Ultimate Guide to Card Game Mastery',
-    description:
-      'Dive into strategies, rules, and expert tips to become a card game champion. Perfect for players of all levels!',
-    type: 'article',
-    url: `${website}/rounds`,
-    images: [
-      {
-        url: `${website}/api/og?title=The Ultimate Guide to Card Game Mastery`,
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'The Ultimate Guide to Card Game Mastery',
-    description:
-      'Unlock pro-level skills in your favorite card games. Strategies, rules, and tips all in one guide!',
-    images: [`${website}/api/og?title=The Ultimate Guide to Card Game Mastery`],
-  },
+type GuessResultSearchParams = {
+  gameId?: string;
+  cardId?: string;
 };
+
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: GuessResultSearchParams;
+}) {
+  const { gameId, cardId } = searchParams;
+
+  const title = 'Dumont - Make Gambling Great Again';
+  const description =
+    'Bet on exciting games like leverageable sports markets and card games in a cheat-proof, verifiable environment powered by blockchain';
+  const url = `https://app.dumont.gg/rounds/${gameId}?cardId=${cardId}`;
+  const imageUrl = `https://app.dumont.gg/api/og?gameId=${gameId}&cardId=${cardId}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'article',
+      url,
+      images: [{ url: imageUrl }],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [imageUrl],
+    },
+  };
+}
 
 export default function RoundPage() {
   return <Round />;
