b25d89578aac57c36b27738a8f4a181f7f4b1ee0
mersedee <mersede.rahbar@gmail.com>
2025-01-19 16:00:52 +0330

    fix: Fix insufficient balance issue when creating game

--- src/views/_components/ConfirmNewRound/index.tsx
+++ src/views/_components/ConfirmNewRound/index.tsx
@@ -1,6 +1,9 @@
-import { Button, DialogDescription, DialogIcon, DialogTitle, Icon } from '@/components';
+import { Button, DialogDescription, DialogIcon, DialogTitle } from '@/components';
+import { useTypedSelector } from '@/hooks/useTypedSelector';
 
 const ConfirmNewRound = ({ onCreateGame }: { onCreateGame: () => void }) => {
+  const { balance } = useTypedSelector((state) => state.account);
+
   return (
     <>
       <DialogIcon name="game-objects-rainbow" variant="default" />
@@ -9,9 +12,11 @@ const ConfirmNewRound = ({ onCreateGame }: { onCreateGame: () => void }) => {
         You need to pay <span className="font-bold">$1</span> to create the round.
       </DialogDescription>
       <div className="flex flex-col gap-4 mt-8">
-        <Button fullWidth size="md" radius="lg" onClick={onCreateGame}>
-          Create
-        </Button>
+        {balance && (
+          <Button fullWidth size="md" radius="lg" onClick={onCreateGame} disabled={balance === '0'}>
+            {balance === '0' ? 'Insufficient Balance' : 'Create'}
+          </Button>
+        )}
 
         <p className="text-neutral-400 text-xs text-center font-thin mt-2">
           *Note: This fee covers blockchain proof costs.
