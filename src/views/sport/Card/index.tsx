4ac123935ac8f8a4b3764bdfdecf36b8d825a4c6
Matin Kaboli <matinkaboli@aol.com>
2025-03-04 12:52:59 +0330

    chore: use props for card component

--- src/views/sport/Card/index.tsx
+++ src/views/sport/Card/index.tsx
@@ -11,57 +11,46 @@ interface ILeague {
 
 interface ITeam {
   name: string;
-  shortName: string;
   logo: string;
+  shortName: string;
 }
 
 interface Props {
-  isActive?: boolean;
-  league?: ILeague;
-  homeTeam?: ITeam;
-  awayTeam?: ITeam;
+  league: ILeague;
+  homeTeam: ITeam;
+  awayTeam: ITeam;
+  isActive: boolean;
+  matchTime: string;
 }
 
-const Card = ({ isActive = false }: Props) => {
+const Card = ({ isActive, league, homeTeam, awayTeam, matchTime }: Props) => {
   return (
     <div className="relative border-[1.5px] border-neutral-700 bg-neutral-800 rounded-lg w-full sm:pt-4 sm:pb-6 sm:px-2 p-3 cursor-pointer">
       {isActive && <PulsingCircle size="sm" className="absolute top-2 left-2" />}
       <div className="text-center text-xs text-neutral-400">Match time</div>
-      <div className="text-center text-sm text-neutral-100 mt-0.5">23 Aug - 23:30</div>
+      <div className="text-center text-sm text-neutral-100 mt-0.5">{matchTime}</div>
 
       <h6 className="flex items-center text-base text-white font-bold mt-3 whitespace-nowrap w-fit mx-auto">
         <span className="inline-flex items-center gap-2">
-          Real Madrid
-          <Image
-            width={0}
-            height={0}
-            className="h-6 w-auto"
-            src="/images/teams/real-madrid.svg"
-            alt=""
-          />
+          {homeTeam.name}
+          <Image width={0} height={0} className="h-6 w-auto" src={homeTeam.logo} alt="" />
         </span>
         <span className="px-2">-</span>
         <span className="inline-flex items-center gap-2">
-          <Image
-            width={0}
-            height={0}
-            className="h-6 w-auto"
-            src="/images/teams/barcelona.svg"
-            alt=""
-          />
-          Barcelona
+          <Image width={0} height={0} className="h-6 w-auto" src={awayTeam.logo} alt="" />
+          {awayTeam.name}
         </span>
       </h6>
 
       <div className="flex gap-1 items-center text-xs text-center text-neutral-400 w-fit mx-auto mt-3">
         <Icon name="ball" width="12" height="12" />
-        Champions league
+        {league.name}
       </div>
 
       <div className="mt-4 flex flex-col gap-3">
-        <ProgressBar name="RMD" percentage={56} bgColor="white" />
+        <ProgressBar name={homeTeam.shortName} percentage={56} bgColor="white" />
         <ProgressBar name="DRAW" percentage={10} />
-        <ProgressBar name="FCB" percentage={34} />
+        <ProgressBar name={awayTeam.shortName} percentage={34} />
       </div>
     </div>
   );
--- src/views/sport/index.tsx
+++ src/views/sport/index.tsx
@@ -2,6 +2,23 @@ import { PulsingCircle } from '@/components';
 
 import Card from './Card';
 
+const BARCELONA = {
+  name: 'Barcelona',
+  shortName: 'FCB',
+  logo: '/images/teams/barcelona.svg',
+};
+
+const REAL_MADRID = {
+  name: 'Real Madrid',
+  shortName: 'RMD',
+  logo: '/images/teams/real-madrid.svg',
+};
+
+const CHAMPIONS_LEAGUE = {
+  name: 'Champions League',
+  logo: '',
+};
+
 const Sport = () => {
   return (
     <>
@@ -17,7 +34,14 @@ const Sport = () => {
       </div>
       <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-3 sm:mt-8 mt-6">
         {[0, 1, 2, 3, 4, 5].map((item) => (
-          <Card key={item} isActive={item % 2 === 0} />
+          <Card
+            key={item}
+            homeTeam={BARCELONA}
+            awayTeam={REAL_MADRID}
+            isActive={item % 2 === 0}
+            league={CHAMPIONS_LEAGUE}
+            matchTime="23 Aug - 23:30"
+          />
         ))}
       </div>
     </>
