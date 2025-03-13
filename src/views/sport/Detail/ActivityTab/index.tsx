1cbbc58382c61334bac25e97fb48853fa863e472
mersedee <mersede.rahbar@gmail.com>
2025-03-13 09:26:34 +0330

    feat: Implement history table

--- src/views/sport/Detail/ActivityTab/index.tsx
+++ src/views/sport/Detail/ActivityTab/index.tsx
@@ -3,16 +3,23 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components';
 
 import Positions from './Positions';
+import History from './History';
 
 const ActivityTab = ({ className = '' }: { className?: string }) => {
   return (
     <Tabs defaultValue="positions" className={className} onChange={(e) => e.preventDefault()}>
       <TabsList>
-        <TabsTrigger value="positions" className="sm:!min-w-[160px]">Active positions</TabsTrigger>
+        <TabsTrigger value="positions" className="sm:!min-w-[160px]">
+          Active positions
+        </TabsTrigger>
         <TabsTrigger value="history">History</TabsTrigger>
       </TabsList>
-      <TabsContent value="positions"><Positions /></TabsContent>
-      <TabsContent value="history">this is two</TabsContent>
+      <TabsContent value="positions">
+        <Positions />
+      </TabsContent>
+      <TabsContent value="history">
+        <History />
+      </TabsContent>
     </Tabs>
   );
 };
