94e3cb7a0285c66c94d237198f00f48f10908190
mersedee <mersede.rahbar@gmail.com>
2025-05-08 13:32:48 +0330

    chore: Upgrade Storybook from v7 to v8

--- src/stories/Toast.stories.tsx
+++ src/stories/Toast.stories.tsx
@@ -48,8 +48,12 @@ export function ContentVariant() {
   return (
     <div className="flex-center gap-2">
       <Button onClick={() => notify({ variant: 'neutral' })}>Neutral</Button>
-      <Button variant="secondary" onClick={() => notify({ variant: 'success' })}>Success</Button>
-      <Button variant="warning" onClick={() => notify({ variant: 'error' })}>Error</Button>
+      <Button variant="secondary" onClick={() => notify({ variant: 'success' })}>
+        Success
+      </Button>
+      <Button variant="warning" onClick={() => notify({ variant: 'error' })}>
+        Error
+      </Button>
       <Toast />
     </div>
   );
