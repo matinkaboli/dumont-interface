c861ec6329dbadf781e66cbeba805c02c8002ab6
mersedee <mersede.rahbar@gmail.com>
2025-05-15 11:39:11 +0330

    feat: Add swap font display to improve initial load time

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,10 @@ import './globals.css';
 import DialogRoot from './DialogRoot';
 import ConfettiRoot from './ConfettiRoot';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Dumont | Provably fair gambling card game',
