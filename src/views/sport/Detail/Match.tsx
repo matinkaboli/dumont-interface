dbc21669b5a23e9303a11f92a52ec4e2e46cfe10
mersedee <mersede.rahbar@gmail.com>
2025-07-29 12:10:39 +0330

    fix: Fix match display in detail page

--- src/views/sport/Detail/Match.tsx
+++ src/views/sport/Detail/Match.tsx
@@ -39,22 +39,24 @@ const Match = ({ match, className }: Props) => {
         </span>
       </h1>
 
-      <div className='max-w-[541px] flex gap-1.5 w-full mx-auto mt-6'>
-        <ProgressBar
-          name={homeTeam.shortName}
-          percentage={latestOdds?.home ?? 0}
-          bgColor='white'
-          roundedFull
-          labelClassName='text-center'
-        />
-        <ProgressBar name='DRAW' percentage={latestOdds?.draw ?? 0} roundedFull labelClassName='text-center' />
-        <ProgressBar
-          name={awayTeam.shortName}
-          percentage={latestOdds?.away ?? 0}
-          roundedFull
-          labelClassName='text-center'
-        />
-      </div>
+      {latestOdds && (
+        <div className='max-w-[541px] flex gap-1.5 w-full mx-auto mt-6'>
+          <ProgressBar
+            name={homeTeam.shortName}
+            percentage={latestOdds.home}
+            bgColor='white'
+            roundedFull
+            labelClassName='text-center'
+          />
+          <ProgressBar name='DRAW' percentage={latestOdds.draw} roundedFull labelClassName='text-center' />
+          <ProgressBar
+            name={awayTeam.shortName}
+            percentage={latestOdds.away}
+            roundedFull
+            labelClassName='text-center'
+          />
+        </div>
+      )}
     </div>
   );
 };
