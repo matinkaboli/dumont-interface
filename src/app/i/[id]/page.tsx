664e135cb7a9f9bbcf6d81927b5d241b0fa424b0
mersedee <mersede.rahbar@gmail.com>
2025-08-06 14:25:55 +0330

    feat: Remove referral app router

--- src/app/i/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import Home from '@/views/card/Home';
-import { constructMetadata } from '@/app/metadata.config';
-
-export const metadata = constructMetadata();
-
-export default function HomePage() {
-  return <Home />;
-}
