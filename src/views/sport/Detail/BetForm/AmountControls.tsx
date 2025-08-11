c30cff603d2035e9439740806558476e02a9b8b0
Matin Kaboli <matinkaboli@aol.com>
2025-08-11 21:31:56 +0330

    chore: convert percentage to $

--- src/views/sport/Detail/BetForm/AmountControls.tsx
+++ src/views/sport/Detail/BetForm/AmountControls.tsx
@@ -1,7 +1,15 @@
 import { Control, FieldErrors, UseFormSetValue } from 'react-hook-form';
 import Image from 'next/image';
 
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue, Slider } from '@/components';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Slider,
+} from '@/components';
 import { useTypedSelector } from '@/hooks/useTypedSelector';
 import formatDecimal from '@/helpers/formatDecimal';
 import { Outcome } from '@/constants/static';
@@ -23,16 +31,15 @@ interface Props {
   isDisable?: boolean;
 }
 
-const AmountControls = (
-  {
-    control,
-    touchedFields,
-    errors,
-    setValue,
-    defaultMultiplierValue,
-    betDetails,
-    isDisable = false,
-  }: Props) => {
+const AmountControls = ({
+  control,
+  touchedFields,
+  errors,
+  setValue,
+  defaultMultiplierValue,
+  betDetails,
+  isDisable = false,
+}: Props) => {
   const { match } = useTypedSelector((state) => state.match.main);
   const { balance } = useTypedSelector((state) => state.account);
   const { minBetAmount, maxBetAmount } = useTypedSelector((state) => state.faro.bet);
@@ -86,19 +93,19 @@ const AmountControls = (
       value: `${Outcome.Home}`,
       label: match?.homeTeam.name,
       logo: match?.homeTeam.logo,
-      price: `$${match?.latestOdds?.home ?? 0}`,
+      price: `$${((match?.latestOdds?.home ?? 0) / 100).toFixed(2)}`,
     },
     {
       value: `${Outcome.Draw}`,
       label: 'Draw',
       logo: '/images/draw.png',
-      price: `$${match?.latestOdds?.draw ?? 0}`,
+      price: `$${((match?.latestOdds?.draw ?? 0) / 100).toFixed(2)}`,
     },
     {
       value: `${Outcome.Away}`,
       label: match?.awayTeam.name,
       logo: match?.awayTeam.logo,
-      price: `$${match?.latestOdds?.away ?? 0}`,
+      price: `$${((match?.latestOdds?.away ?? 0) / 100).toFixed(2)}`,
     },
   ];
 
@@ -128,24 +135,24 @@ const AmountControls = (
         onValueChange={(value) => setValue('outcome', +value)}
         disabled={isDisable}
       >
-        <SelectTrigger className='w-full'>
-          <SelectValue placeholder='Select a option' />
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="Select a option" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
             {options.map(({ value, label, logo, price }) => (
               <SelectItem key={value} value={value}>
-                <div className='flex items-center gap-1'>
+                <div className="flex items-center gap-1">
                   <Image
                     width={0}
                     height={0}
-                    sizes='100vw'
-                    className='h-6 w-auto'
+                    sizes="100vw"
+                    className="h-6 w-auto"
                     src={logo ?? '/images/draw.png'}
                     alt={label ?? ''}
                   />
                   {label}
-                  <div className='text-xs text-white font-bold bg-primary-700 rounded-full py-0.5 px-1.5'>
+                  <div className="text-xs text-white font-bold bg-primary-700 rounded-full py-0.5 px-1.5">
                     {price}
                   </div>
                 </div>
@@ -166,7 +173,7 @@ const AmountControls = (
           defaultValue={[defaultMultiplierValue]}
           max={30}
           step={1}
-          className='my-5'
+          className="my-5"
           disabled={isDisable}
           onValueChange={(values) => setValue('multiplier', values[0])}
         />
