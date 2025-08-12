9830d682f0ac22666012e5545c80b9e9e6dbfe33
mersedee <mersede.rahbar@gmail.com>
2025-08-12 15:19:08 +0330

    fix: Fix conflict

--- src/components/QRCode/index.tsx
+++ src/components/QRCode/index.tsx
@@ -1,9 +1,32 @@
-import { QRCodeSVG } from 'qrcode.react';
+import { ReactQRCode } from '@lglab/react-qr-code';
 import clsx from 'clsx';
-import { ComponentPropsWithRef } from 'react';
 
-const QRCode = ({ ref, className, ...props }: ComponentPropsWithRef<typeof QRCodeSVG>) => (
-  <QRCodeSVG className={clsx('bg-white p-3 rounded-xl', className)} ref={ref} {...props} />
+interface Props {
+  size?: number;
+  className?: string;
+  value: string;
+  imageSrc: string;
+}
+
+const QRCode = ({ value, size, imageSrc, className }: Props) => (
+  <div className={clsx('w-fit rounded-3xl overflow-hidden', className)}>
+    <ReactQRCode
+      value={value}
+      size={size}
+      background={'#FFFFFF'}
+      dataModulesSettings={{ style: 'diamond' }}
+      finderPatternOuterSettings={{ style: 'inpoint-sm' }}
+      finderPatternInnerSettings={{ style: 'rounded-sm' }}
+      marginSize={2}
+      level='Q'
+      imageSettings={{
+        src: imageSrc,
+        width: 23,
+        height: 23,
+        excavate: true,
+      }}
+    />
+  </div>
 );
 QRCode.displayName = 'QRCode';
 export default QRCode;
